707b8e6ea4bbe129803985719585139b31b1ceae
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-06-08 13:30:43 +0200

    Update segment_lidar.py

--- segment_lidar/segment_lidar.py
+++ segment_lidar/segment_lidar.py
@@ -2,18 +2,16 @@ import os
 import yaml
 import click
 import time
-import json
 import torch
 import cv2
 import numpy as np
 import supervision as sv
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
-import pdal
-from samgeo import SamGeo, tms_to_geotiff
-from osgeo import gdal
+from samgeo import SamGeo
+from samgeo.text_sam import LangSAM
+import rasterio
 import laspy
-from PIL import Image
-gdal.UseExceptions()
+
 
 def cloud_to_image(points, minx, maxx, miny, maxy, resolution):
     width = int((maxx - minx) / resolution) + 1
@@ -40,12 +38,16 @@ def cloud_to_image(points, minx, maxx, miny, maxy, resolution):
 def image_to_cloud(points, minx, maxy, image, resolution):
     segment_ids = []
     unique_values = {}
+    image = np.asarray(image)
 
     for point in points:
         x, y, *_ = point
         pixel_x = int((x - minx) / resolution)
         pixel_y = int((maxy - y) / resolution)
-        rgb = tuple(image[pixel_y, pixel_x])
+        if not (0 <= pixel_x < image.shape[1]) or not (0 <= pixel_y < image.shape[0]):
+            segment_ids.append(-1)
+            continue
+        rgb = image[pixel_y, pixel_x]
 
         if rgb not in unique_values:
             unique_values[rgb] = len(unique_values)
@@ -59,6 +61,48 @@ def image_to_cloud(points, minx, maxy, image, resolution):
 @click.group(help="A package for segmenting LiDAR data using Segment-Anything from Meta AI Research.")
 def cli():
     pass
+
+
+@click.command()
+@click.option('--output', '-o', type=click.Path(exists=False), required=False, help='Output path')
+
+def create_config(output):
+    configuration = {
+        "device": "cuda:0",
+        "algorithm": "segment-geospatial",
+        "model_type": "vit_h",
+        "model_path": "sam_vit_h_4b8939.pth",
+        "sam_kwargs": {
+            "points_per_side": 32,
+            "pred_iou_thresh": 0.9,
+            "stability_score_thresh": 0.92,
+            "crop_n_layers": 1,
+            "crop_n_points_downscale_factor": 1,
+            "min_mask_region_area": 10000
+        },
+        "sam_geo": {
+            "automatic": True,
+            "erosion_kernel_size": 3,
+            "sam_kwargs": False,
+            "text_prompt": None,
+            "box_threshold": 0.24,
+            "text_threshold": 0.30
+        },
+        "input_path": "pointcloud.las",
+        "output_path": "classified.las",
+        "classification": None,
+        "image_path": "raster.tif",
+        "resolution": 0.15
+    }
+
+    yaml_data = yaml.dump(configuration)
+    output = output if output else 'config.yaml'
+
+    with open(output, 'w') as file:
+        file.write(yaml_data)
+
+    print(f'Configuration file created at {output}.')
+
 
 @cli.command()
 @click.option('--input', '-i', type=click.Path(exists=True), required=False, help='Input LiDAR data')
@@ -113,23 +157,23 @@ def segment(input, output, config):
     miny = np.min(points[:, 1])
     maxy = np.max(points[:, 1])
     image = cloud_to_image(points, minx, maxx, miny, maxy, resolution)
-
     image = np.asarray(image).astype(np.uint8)
-    geotransform = (minx, resolution, 0, miny, 0, -resolution)
 
-    driver = gdal.GetDriverByName('GTiff')
-    out_ds = driver.Create(image_path, image.shape[1], image.shape[0], 3, gdal.GDT_Byte)
-    out_ds.SetGeoTransform(geotransform)
-    for i in range(3):
-        band = out_ds.GetRasterBand(i + 1)
-        band.WriteArray(image[:, :, i])
-    out_ds = None
+    with rasterio.open(
+        image_path,
+        'w',
+        driver='GTiff',
+        width=image.shape[1],
+        height=image.shape[0],
+        count=3,
+        dtype=image.dtype
+    ) as dst:
+        for i in range(3):
+            dst.write(image[:, :, i], i + 1)
 
     # Instance segmentation
-    gdal.AllRegister()
-    src = gdal.Open(image_path, 1)
-    band = src.GetRasterBand(1)
-    image_rgb = src.ReadAsArray()
+    with rasterio.open(image_path, 'r') as src:
+        image_rgb = src.read()
 
     output_path = output if output else cnfg['output_path']
     if cnfg['algorithm'] == 'segment-anything':
@@ -148,6 +192,7 @@ def segment(input, output, config):
             min_mask_region_area=mask['min_mask_region_area']
         )
 
+        image_rgb = image_rgb.reshape((image_rgb.shape[1], image_rgb.shape[2], image_rgb.shape[0]))
         result = mask_generator.generate(image_rgb)
         mask_annotator = sv.MaskAnnotator()
         detections = sv.Detections.from_sam(result)
@@ -157,13 +202,27 @@ def segment(input, output, config):
         cv2.imwrite(f"{os.path.dirname(output_path)}/labeled.tif", annotated_image)
 
     elif cnfg['algorithm'] == 'segment-geospatial':
-        sam = SamGeo(
-            model_type=model_type,
-            checkpoint=model_path,
-            sam_kwargs=mask
-        )
+        # Parameters
+        sam_kwargs = mask if cnfg['sam_geo']['sam_kwargs'] else None
+        automatic = cnfg['sam_geo']['automatic']
+        text_prompt = cnfg['sam_geo']['text_prompt']
+        box_threshold = cnfg['sam_geo']['box_threshold']
+        text_threshold = cnfg['sam_geo']['text_threshold']
+        erosion_kernel = (cnfg['sam_geo']['erosion_kernel_size'], cnfg['sam_geo']['erosion_kernel_size']) if cnfg['sam_geo']['erosion_kernel_size'] else None
 
-        sam.generate(source=image_path, output=f"{os.path.dirname(output_path)}/labeled.tif", foreground=True, unique=True)
+        # Segmentation
+        if text_prompt is not None:
+            sam = LangSAM()
+            sam.predict(image=image_path, text_prompt=text_prompt, box_threshold=box_threshold, text_threshold=text_threshold, output=f"{os.path.dirname(output_path)}/labeled.tif",)
+
+        else:
+            sam = SamGeo(
+                model_type=model_type,
+                checkpoint=model_path,
+                automatic=automatic,
+                sam_kwargs=sam_kwargs
+            )
+            sam.generate(source=image_path, output=f"{os.path.dirname(output_path)}/labeled.tif", erosion_kernel=erosion_kernel, foreground=True, unique=True)
 
     # Project results on point cloud
     extension = os.path.splitext(output_path)[1]
@@ -177,7 +236,11 @@ def segment(input, output, config):
         print(error_lines[-1])
         exit()
 
-    segment_ids = image_to_cloud(points, f"{os.path.dirname(output_path)}/labeled.tif", minx, maxy, resolution)
+    with rasterio.open(os.path.dirname(output_path) + "/labeled.tif", 'r') as src:
+        segmented_image = src.read()
+        segmented_image = np.squeeze(segmented_image)
+
+    segment_ids = image_to_cloud(points, minx, maxy, segmented_image, resolution)
     lidar.add_extra_dim(laspy.ExtraBytesParams(name="segment_id", type=np.int32))
     lidar.segment_id = segment_ids
 
@@ -186,6 +249,8 @@ def segment(input, output, config):
     end = time.time()
     print(f'Point cloud segmentation completed in {np.round(end - start, 2)} seconds.')
 
+
+cli.add_command(create_config)
 cli.add_command(segment)
 
 if __name__ == '__main__':
