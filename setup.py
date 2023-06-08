2a5009ec3d9481e2752aca00824aaa2d0c662c63
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-06-08 14:14:28 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name="segment-lidar",
+    version='0.1.0',
+    description="A package for segmenting LiDAR data using Segment-Anything Model (SAM) from Meta AI Research.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='BSD 3-Clause "New" or "Revised" License',
+    author = 'Anass Yarroudh',
+    author_email = 'ayarroudh@uliege.be',
+    url = 'https://github.com/Yarroudh/segment-lidar',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "segment_lidar=segment_lidar.main:cli"
+        ]
+    }
+)
