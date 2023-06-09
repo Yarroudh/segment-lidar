f6e252f6363afc6e3db3ad5048c1f1ea81771d01
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-06-09 12:37:53 +0200

    Update main.py

--- segment_lidar/main.py
+++ segment_lidar/main.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 - University of Liège
+# Author : Anass Yarroudh (ayarroudh@uliege.be), Geomatics Unit of ULiege
+# This file is distributed under the BSD-3 licence. See LICENSE file for complete text of the license.
+
 import os
 import yaml
 import click
