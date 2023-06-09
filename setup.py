3c9948b97b125652b88ba198e63ea927474d9fef
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-06-09 21:57:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as file:
 
 setup(
     name="segment-lidar",
-    version='0.1.1',
+    version='0.1.2',
     description="A package for segmenting LiDAR data using Segment-Anything Model (SAM) from Meta AI Research.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
